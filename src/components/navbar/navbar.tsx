dd15154ff15a561734e3aef55b3fe84a829b6de5
duckuru <sivlong42069@gmail.com>
2025-09-19 15:12:13 +0700

    search just need backend

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -11,45 +11,109 @@ import {
 } from "../ui/dialog";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
 // import { plusIcon } from '@fortawesome/'
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
 
-export default function Navbar(props: { onLogin: any; onSignup: any; user: any; loginOpen: any; setLoginOpen: any; signupOpen: any; setSignupOpen: any, onPost: any; postOpen: any; setPostOpen: any; }) {
+export default function Navbar(props: {
+  onLogin: any;
+  onSignup: any;
+  user: any;
+  loginOpen: any;
+  setLoginOpen: any;
+  signupOpen: any;
+  setSignupOpen: any;
+  onPost: any;
+  postOpen: any;
+  setPostOpen: any;
+}) {
   //TODO: show error message, especially "name already exist";
   //extract prop
-  const { onLogin, onSignup, user, loginOpen, setLoginOpen, signupOpen, setSignupOpen, onPost, postOpen, setPostOpen } = props;
+  const {
+    onLogin,
+    onSignup,
+    user,
+    loginOpen,
+    setLoginOpen,
+    signupOpen,
+    setSignupOpen,
+    onPost,
+    postOpen,
+    setPostOpen,
+  } = props;
 
   //login usestate
-  const [loginUsername, setLoginUsername] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
   //signup usestate
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [conpassword, setConPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [conpassword, setConPassword] = useState("");
+
+  const [searchParam, setSearchParam] = useState("");
+  const [category, setCategory] = useState("");
 
   //news useState
-  const [headline, setHeadline] = useState('');
-  const [content, setContent] = useState('');
+  const [headline, setHeadline] = useState("");
+  const [content, setContent] = useState("");
 
-  const handleLoginClick = (e: { preventDefault: () => void; }) => {
+  const handleLoginClick = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     onLogin(loginUsername, loginPassword);
-  }
+  };
 
-  const handleSignupClick = (e: { preventDefault: () => void; }) => {
+  const handleSignupClick = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     onSignup(username, password, conpassword);
-  }
+  };
 
-  const handleNewsPost = (e: { preventDefault: () => void; }) => {
+  const handleNewsPost = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     onPost(headline, content);
-  }
+  };
+
+  const searchWithCategory = async (e?: React.FormEvent) => {
+    if (e) e.preventDefault(); // stop form from reloading page
+    if (!searchParam && !category) {
+      alert("Please enter a search term or select a category.");
+      return;
+    }
+
+    console.log("Searching with:", {
+      query: searchParam,
+      category: category || "all",
+    });
+
+    try {
+      const response = await fetch(
+        `http://localhost:8080/search?query=${encodeURIComponent(
+          searchParam
+        )}&category=${category}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch search results");
+      }
+
+      const data = await response.json();
+      console.log("Search Results:", data);
+      // TODO: Pass this data to a parent state or display it on the page
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="sticky top-0 z-50 w-screen bg-[#f3f3f3] px-8 shadow-md h-24 flex justify-between items-center">
@@ -60,29 +124,42 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
 
       {/* Search */}
       <div className="flex gap-4 items-center">
-        <Input placeholder="Search for news" className="w-[25rem] h-12 border-2" />
-        <Select>
+        <Input
+          placeholder="Search for news"
+          className="w-[25rem] h-12 border-2"
+          value={searchParam}
+          onChange={(e) => setSearchParam(e.target.value)}
+        />
+        <Select onValueChange={(value) => setCategory(value)}>
           <SelectTrigger className="w-[12rem] border-2 h-12">
             <SelectValue placeholder="Select a Category" />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
+              <SelectLabel>Categories</SelectLabel>
+              <SelectItem value="business">Business</SelectItem>
+              <SelectItem value="entertainment">Entertainment</SelectItem>
+              <SelectItem value="general">General</SelectItem>
+              <SelectItem value="health">Health</SelectItem>
+              <SelectItem value="science">Science</SelectItem>
+              <SelectItem value="sports">Sports</SelectItem>
+              <SelectItem value="technology">Technology</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
-        <Button className="bg-[#f3f3f3] hover:bg-[#e1e1e1] border-2 h-12 w-12 cursor-pointer">
-          <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "#3f3f3f" }} />
+        <Button
+          className="bg-[#f3f3f3] hover:bg-[#e1e1e1] border-2 h-12 w-12 cursor-pointer"
+          onClick={searchWithCategory}
+        >
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            style={{ color: "#3f3f3f" }}
+          />
         </Button>
       </div>
-      {user.user ?
+      {user.user ? (
         <>
-          <Dialog  open={postOpen} onOpenChange={setPostOpen}>
+          <Dialog open={postOpen} onOpenChange={setPostOpen}>
             <DialogTrigger asChild>
               <FontAwesomeIcon icon={faPlus} style={{ color: "#3f3f3f" }} />
               {/* <Button variant="outline" className="text-xl px-6 py-3 mr-3" onClick={() => setLoginOpen(true)}>Login</Button> */}
@@ -90,8 +167,12 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
 
             <DialogContent className="sm:max-w-[425px]">
               <DialogHeader>
-                <DialogTitle className="text-center text-3xl">Create Post</DialogTitle>
-                <DialogDescription>Upload your news to the world!</DialogDescription>
+                <DialogTitle className="text-center text-3xl">
+                  Create Post
+                </DialogTitle>
+                <DialogDescription>
+                  Upload your news to the world!
+                </DialogDescription>
               </DialogHeader>
 
               <form className="text-[#3f3f3f] grid gap-4">
@@ -100,38 +181,61 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                   <Input
                     id="news-headline"
                     value={headline}
-                    onChange={e => setHeadline(e.target.value)}
+                    onChange={(e) => setHeadline(e.target.value)}
                     autoFocus
                     maxLength={60}
                   />
                 </div>
                 <div className="grid gap-3">
                   <Label htmlFor="news-content">Content</Label>
-                  <Textarea id="news-content" className="resize-none" maxLength={300} rows={6} value={content} onChange={e => setContent(e.target.value)} />
+                  <Textarea
+                    id="news-content"
+                    className="resize-none"
+                    maxLength={300}
+                    rows={6}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
                 </div>
                 <DialogFooter className="grid gap-2 w-full">
-                  <Button className="w-full p-3" onClick={handleNewsPost}>Upload</Button>
+                  <Button className="w-full p-3" onClick={handleNewsPost}>
+                    Upload
+                  </Button>
                 </DialogFooter>
               </form>
             </DialogContent>
           </Dialog>
-          <Link to='/profile'>
-            <img src="/vite.svg" alt="" className="border border-black rounded-full p-2" />
+          <Link to="/profile">
+            <img
+              src="/vite.svg"
+              alt=""
+              className="border border-black rounded-full p-2"
+            />
           </Link>
         </>
-        :
+      ) : (
         <>
           {/* Auth Dialog (Login/Signup toggle) */}
           <span className="auth-btn">
             <Dialog open={loginOpen} onOpenChange={setLoginOpen}>
               <DialogTrigger asChild>
-                <Button variant="outline" className="text-xl px-6 py-3 mr-3" onClick={() => setLoginOpen(true)}>Login</Button>
+                <Button
+                  variant="outline"
+                  className="text-xl px-6 py-3 mr-3"
+                  onClick={() => setLoginOpen(true)}
+                >
+                  Login
+                </Button>
               </DialogTrigger>
 
               <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
-                  <DialogTitle className="text-center text-3xl">Login</DialogTitle>
-                  <DialogDescription>Enter your username and password to log in.</DialogDescription>
+                  <DialogTitle className="text-center text-3xl">
+                    Login
+                  </DialogTitle>
+                  <DialogDescription>
+                    Enter your username and password to log in.
+                  </DialogDescription>
                 </DialogHeader>
 
                 <form className="text-[#3f3f3f] grid gap-4">
@@ -140,7 +244,7 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                     <Input
                       id="login-username"
                       value={loginUsername}
-                      onChange={e => setLoginUsername(e.target.value)}
+                      onChange={(e) => setLoginUsername(e.target.value)}
                       autoFocus
                     />
                   </div>
@@ -150,11 +254,13 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                       id="login-password"
                       type="password"
                       value={loginPassword}
-                      onChange={e => setLoginPassword(e.target.value)}
+                      onChange={(e) => setLoginPassword(e.target.value)}
                     />
                   </div>
                   <DialogFooter className="grid gap-2 w-full">
-                    <Button className="w-full p-3" onClick={handleLoginClick}>Login</Button>
+                    <Button className="w-full p-3" onClick={handleLoginClick}>
+                      Login
+                    </Button>
                     <Button
                       type="button"
                       variant="ghost"
@@ -163,7 +269,8 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                         setLoginOpen(false);
                         setSignupOpen(true);
                       }}
-                    >Don't have an account? Sign Up
+                    >
+                      Don't have an account? Sign Up
                     </Button>
                   </DialogFooter>
                 </form>
@@ -173,12 +280,20 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
             {/* sign up button   */}
             <Dialog open={signupOpen} onOpenChange={setSignupOpen}>
               <DialogTrigger asChild>
-                <Button variant="outline" className="text-xl px-6 py-3" onClick={() => setSignupOpen(true)}>Sign Up</Button>
+                <Button
+                  variant="outline"
+                  className="text-xl px-6 py-3"
+                  onClick={() => setSignupOpen(true)}
+                >
+                  Sign Up
+                </Button>
               </DialogTrigger>
 
               <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
-                  <DialogTitle className="text-center text-3xl">Sign Up</DialogTitle>
+                  <DialogTitle className="text-center text-3xl">
+                    Sign Up
+                  </DialogTitle>
                   <DialogDescription>Create your account</DialogDescription>
                 </DialogHeader>
 
@@ -188,7 +303,7 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                     <Input
                       id="signup-username"
                       value={username}
-                      onChange={e => setUsername(e.target.value)}
+                      onChange={(e) => setUsername(e.target.value)}
                       autoFocus
                     />
                   </div>
@@ -198,7 +313,7 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                       id="signup-password"
                       type="password"
                       value={password}
-                      onChange={e => setPassword(e.target.value)}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
                   <div className="grid gap-3">
@@ -207,11 +322,13 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                       id="signup-confirm"
                       type="password"
                       value={conpassword}
-                      onChange={e => setConPassword(e.target.value)}
+                      onChange={(e) => setConPassword(e.target.value)}
                     />
                   </div>
                   <DialogFooter className="grid gap-2 w-full">
-                    <Button className="w-full py-3" onClick={handleSignupClick}>Register</Button>
+                    <Button className="w-full py-3" onClick={handleSignupClick}>
+                      Register
+                    </Button>
                     <Button
                       type="button"
                       variant="ghost"
@@ -219,7 +336,9 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
                       onClick={() => {
                         setSignupOpen(false);
                         setLoginOpen(true);
-                      }}>Already have an account? Login
+                      }}
+                    >
+                      Already have an account? Login
                     </Button>
                   </DialogFooter>
                 </form>
@@ -227,7 +346,7 @@ export default function Navbar(props: { onLogin: any; onSignup: any; user: any; 
             </Dialog>
           </span>
         </>
-      }
+      )}
     </div>
   );
 }
